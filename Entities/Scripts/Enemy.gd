5216b27e9e03797ac768b315772b1132c2920f45
Maximilian Nasert <maximilian.nasert@gmx.de>
2023-08-27 23:09:14 +0200

    push again

--- Entities/Scripts/Enemy.gd
+++ Entities/Scripts/Enemy.gd
@@ -14,7 +14,7 @@ func move(delta):
 	if self.target == null:
 		self.target = get_tree().get_nodes_in_group("Player")[0]
 		return
-	var movevec = (self.target.position - self.position).normalized() * self.speed
+	var movevec = (self.target.position - self.position).normalized()
 	self.position += self.speed * movevec * delta
 
 func _process(delta):
--- Entities/Scripts/Player.gd
+++ Entities/Scripts/Player.gd
@@ -7,7 +7,7 @@ extends Node2D
 
 @export var bullet: PackedScene
 
-var can_shoot = true
+var can_shoot = false
 var firerate_cooldown = 1/firerate  # cooldown in s
 # Called when the node enters the scene tree for the first time.
 func _ready():
